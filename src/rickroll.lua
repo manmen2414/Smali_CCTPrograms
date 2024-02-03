b6d61caf91b03467403c1823058a2ff13246d9b3
2 1 <InotitoKokoro@gmail.com>
2024-02-03 22:21:06 +0900

    Update rickroll.lua

--- src/rickroll.lua
+++ src/rickroll.lua
@@ -1,2 +1,4 @@
-shell.execute("speaker", "play",
-    "https://cdn.discordapp.com/attachments/1175720574690021446/1203319582169894932/Rick-Astley-Never-Gonna-Give-You-Up-_Official-Music-Video_-128.dfpwm")
+while true do
+    shell.execute("speaker", "play",
+        "https://cdn.discordapp.com/attachments/1175720574690021446/1203319582169894932/Rick-Astley-Never-Gonna-Give-You-Up-_Official-Music-Video_-128.dfpwm")
+end
