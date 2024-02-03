72f23759befc783a270bfc0c49d4d3334bff398c
2 1 <InotitoKokoro@gmail.com>
2024-02-03 22:11:35 +0900

    Update rickroll.lua

--- src/rickroll.lua
+++ src/rickroll.lua
@@ -1,11 +1,2 @@
-local dfpwm = require("cc.audio.dfpwm")
-local speaker = peripheral.find("speaker")
-
-local decoder = dfpwm.make_decoder()
-for chunk in io.lines("https://cdn.discordapp.com/attachments/1175720574690021446/1203305783014719498/RickRoll_Chaim2.dfpwm", 16 * 1024) do
-    local buffer = decoder(chunk)
-
-    while not speaker.playAudio(buffer) do
-        os.pullEvent("speaker_audio_empty")
-    end
-end
+shell.execute("speaker", "play",
+    "https://cdn.discordapp.com/attachments/1175720574690021446/1203319582169894932/Rick-Astley-Never-Gonna-Give-You-Up-_Official-Music-Video_-128.dfpwm")
