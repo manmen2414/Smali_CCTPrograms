bfd4811b34ff36eadcb3ce205a273a3394d91e6a
2 1 <InotitoKokoro@gmail.com>
2024-02-03 21:29:46 +0900

    Update rickroll.lua

--- src/rickroll.lua
+++ src/rickroll.lua
@@ -1,3 +1,11 @@
+local dfpwm = require("cc.audio.dfpwm")
 local speaker = peripheral.find("speaker")
-speaker.playAudio(
-    "https://cdn.discordapp.com/attachments/1175720574690021446/1203305783014719498/RickRoll_Chaim2.dfpwm")
+
+local decoder = dfpwm.make_decoder()
+for chunk in io.lines("https://cdn.discordapp.com/attachments/1175720574690021446/1203305783014719498/RickRoll_Chaim2.dfpwm", 16 * 1024) do
+    local buffer = decoder(chunk)
+
+    while not speaker.playAudio(buffer) do
+        os.pullEvent("speaker_audio_empty")
+    end
+end
