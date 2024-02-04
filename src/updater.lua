025760be7023116be0f3deb94c20bc9cd0bd4ce9
2 1 <InotitoKokoro@gmail.com>
2024-02-04 19:26:54 +0900

    Create updater.lua

---
+++ src/updater.lua
@@ -0,0 +1,14 @@
+term.clear()
+term.setCursorPos(1, 1)
+print(">>> Study lua manmen2414 updater <<<")
+print("What do you want to update?")
+print("+ play_music_inf")
+local value = read()
+if value == "play_music_inf" then
+    shell.execute("delete", "play_music_inf.lua")
+    print("[Get] play_music_inf.lua")
+    shell.execute("wget", "https://raw.githubusercontent.com/manmen2414/study_lua_manmen2414/main/src/play_music_inf.lua")
+    print("----- END -----")
+else
+    print("Your select is wrong.")
+end
