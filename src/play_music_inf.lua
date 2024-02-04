2e33c9640266f080ab908910231594f31cf2c563
2 1 <InotitoKokoro@gmail.com>
2024-02-04 19:02:53 +0900

    debug

--- src/play_music_inf.lua
+++ src/play_music_inf.lua
@@ -8,19 +8,27 @@ while true do
     local musicurl = read()
     if not musicurl == "end" then
         musics[#musics + 1] = musicurl
+        print(1)
     else
+        print(2)
         break
     end
+    print(3)
     sleep(0.5)
 end
 local i = 0
 
 while true do
     for index, value in next, musics do
+        print(4)
         print(index .. ": " .. value + 1)
         term.clear()
         print("I am playing " .. i .. " time(s).")
         shell.execute("speaker", "play", value)
+        print(5)
     end
+    print(6)
+    sleep(0.5)
     i = i + 1
+    print(7)
 end
