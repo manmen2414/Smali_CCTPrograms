70c2bccefcb17dc43164d798e97425fd4b9fb20c
2 1 <InotitoKokoro@gmail.com>
2024-02-04 18:50:31 +0900

    Update play_music_inf.lua

--- src/play_music_inf.lua
+++ src/play_music_inf.lua
@@ -1,12 +1,25 @@
-term.clear()
-term.setCursorPos(1, 1)
-print("URL:")
-local url = read()
+local musics = {}
+while true do
+    term.clear()
+    term.setCursorPos(1, 1)
+    print("You can get .dfpwm file on \nhttps://music.madefor.cc/")
+    print("You have " .. #musics .. " music(s)")
+    print("If you type 'end', then play music. URLs:")
+    local musicurl = read()
+    if not musicurl == "end" then
+        musics[#musics + 1] = musicurl
+    else
+        break
+    end
+end
 local i = 0
 
 while true do
+    for index, value in next, musics do
+        print(index .. ": " .. value + 1)
+        term.clear()
+        print("I am playing " .. i .. " time(s).")
+        shell.execute("speaker", "play", value)
+    end
     i = i + 1
-    term.clear()
-    print("I am playing " .. i .. " time(s).")
-    shell.execute("speaker", "play", url)
 end
