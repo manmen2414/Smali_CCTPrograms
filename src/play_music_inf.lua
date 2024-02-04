8fee6a66f57f682bc06506aec59d37443fbb65ae
2 1 <InotitoKokoro@gmail.com>
2024-02-04 19:53:03 +0900

    Update play_music_inf.lua

--- src/play_music_inf.lua
+++ src/play_music_inf.lua
@@ -4,11 +4,41 @@ while true do
     term.setCursorPos(1, 1)
     print("You can get .dfpwm file on \nhttps://music.madefor.cc/")
     print("You have " .. #musics .. " music(s)")
-    print("If you type 'end', then play music. URLs:")
+    print("Type 'end':Play")
+    print("Type 'save':Save Playlist")
+    print("Type 'load':Load Playlist")
+    print("Please URL or command:")
     local musicurl = read()
     if musicurl == "end" then
         --print(4)
         break
+    elseif musicurl == "save" then
+        local text = ""
+        print("Please type name")
+        local path = read()
+        for index, value in next, musics do
+            text = text .. "\n" .. value
+            print("Writing music" .. index)
+        end
+        local file = fs.open(path, "w")
+        file.write(text)
+        file.close()
+        print("-End!")
+    elseif musicurl == "load" then
+        term.clear()
+        term.setCursorPos(1, 1)
+        print("Please type .mplaylist path")
+        local path = read()
+        local file = fs.open(path, "r")
+        local lines = {}
+        while true do
+            local line = file.readLine()
+            if not line then break end
+            musics[#musics + 1] = line
+            print("Adding music...")
+        end
+        file.close()
+        print("-End!")
     else
         musics[#musics + 1] = musicurl
         print("Adding music...")
@@ -22,6 +52,7 @@ while true do
     for index, value in next, musics do
         --print(4)
         term.clear()
+        term.setCursorPos(1, 1)
         print("I am playing " .. i .. " time(s).\nhold CTRL+R to end")
         shell.execute("speaker", "play", value)
         --print(5)
