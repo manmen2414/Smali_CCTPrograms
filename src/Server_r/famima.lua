5c08f7afc0657692dd390080d6ef765e3da2d285
2 1 <InotitoKokoro@gmail.com>
2024-02-05 21:55:52 +0900

    Update famima.lua

--- src/Server_r/famima.lua
+++ src/Server_r/famima.lua
@@ -1,6 +1,7 @@
 local count = 0
 while true do
     if redstone.getInput("front") then
+        count = count + 1
         shell.execute("speaker", "play",
             "https://cdn.discordapp.com/attachments/1204033389200678953/1204036147429507072/-.dfpwm")
         sleep(1)
