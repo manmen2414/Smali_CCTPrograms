3289f485555236f7a8191f123a8534c8c27900be
2 1 <InotitoKokoro@gmail.com>
2024-02-05 21:53:37 +0900

    Create famima.lua

---
+++ src/Server_r/famima.lua
@@ -0,0 +1,12 @@
+local count = 0
+repeat
+    if redstone.getInput("front") then
+        shell.execute("speaker", "play",
+            "https://cdn.discordapp.com/attachments/1204033389200678953/1204036147429507072/-.dfpwm")
+        sleep(1)
+    end
+    term.clear()
+    term.setCursorPos(1, 1)
+    print("Count : " .. count)
+    sleep(0.5)
+until true
