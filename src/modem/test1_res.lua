bc76a9e9e2d9426489489ef0ba3bbdb64da37068
manmen2414 <102225383+manmen2414@users.noreply.github.com>
2024-02-07 10:32:44 +0900

    Update test1_res.lua

--- src/modem/test1_res.lua
+++ src/modem/test1_res.lua
@@ -1,1 +1,11 @@
---create
+local modem = peripheral.find("modem") or error("No modem attached", 0)
+modem.open(15)
+local event, side, channel,replyChannel, message, distance
+repeat
+  event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
+until channel == 15
+print("Received a reply: " .. tostring(message))
+modem.transmit(43, 15, "Hello, world!")
+
+
+
