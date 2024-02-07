2193e74dba98e5b242f8defa048326bef467ada3
manmen2414 <102225383+manmen2414@users.noreply.github.com>
2024-02-07 11:42:00 +0900

    Add files via upload

---
+++ src/modem/radio_send.lua
@@ -0,0 +1,78 @@
+local modem = peripheral.find("modem") or error("No modem attached", 0)
+local send = 28
+local receive = 72
+modem.open(send)
+local musics = {}
+while true do
+    term.clear()
+    term.setCursorPos(1, 1)
+    print("You can get .dfpwm file on \nhttps://music.madefor.cc/")
+    print("You have " .. #musics .. " music(s)")
+    print("Type 'end':Play")
+    print("Type 'save':Save Playlist")
+    print("Type 'load':Load Playlist")
+    print("Please URL or command:")
+    local musicurl = read()
+    if musicurl == "end" then
+        break
+    elseif musicurl == "save" then
+        local text = ""
+        print("Please type name")
+        local path = read()
+        print("[" .. path .. ".mplaylist] OK?(y/n)")
+        local check = read()
+        if check == "y" then
+            for index, value in next, musics do
+                text = text .. "\n" .. value
+                print("Writing music" .. index)
+            end
+            local file = fs.open(path .. ".mplaylist", "w")
+            file.write(text)
+            file.close()
+            print("-End!")
+        end
+    elseif musicurl == "load" then
+        term.clear()
+        term.setCursorPos(1, 1)
+        print("Please type .mplaylist path")
+        local path = read()
+        local file = fs.open(path, "r")
+        local skipped = false
+        while true do
+            local line = file.readLine()
+            if not line then break end
+            if skipped then
+                musics[#musics + 1] = line
+                print("Adding music...")
+            else
+                skipped = true
+            end
+        end
+        file.close()
+        print("-End!")
+    else
+        musics[#musics + 1] = musicurl
+        print("Adding music...")
+    end
+    --print(3)
+    sleep(0.5)
+end
+
+local i = 0
+
+while true do
+    for index, value in next, musics do
+        --print(4)
+        term.clear()
+        term.setCursorPos(1, 1)
+        print("I am playing " .. i .. " time(s).\nhold CTRL+R:end\nhold CTRL+T:next")
+        sleep(0.5)
+        modem.transmit(receive, send, value)
+        shell.execute("speaker", "play", value)
+        --print(5)
+    end
+    --print(6)
+    sleep(0.5)
+    i = i + 1
+    --print(7)
+end
